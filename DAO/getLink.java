d753507c7655dabc19a42aacf7190b018a796da6
Xlawliet <55490555+Xlawliet@users.noreply.github.com>
2019-10-16 14:25:28 +0800

    Create getLink.java

---
+++ DAO/getLink.java
@@ -0,0 +1,50 @@
+package SA_2.DAO;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+//连接db
+public class getLink {
+    private  static  String driverName="com.microsoft.sqlserver.jdbc.SQLServerDriver";//这个不改，全世界都一样
+    private static String user="sa";//用户名
+    private static String pwd="xzy520520";//密码
+    private static  String url="jdbc:sqlserver://127.0.0.1:1433;databaseName=SA_2";//数据库连接。格式：数据库类型+IP+端口+名字
+    public static Connection getLink()
+    {
+        try
+        {
+            Class.forName(driverName);
+            return DriverManager.getConnection(url,user,pwd);    //
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+        return null;
+    }
+    public static void Close(Connection conn)
+    {
+        try
+        {
+            conn.close();
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
+    public static void Close(Connection conn, CallableStatement call)
+    {
+        try
+        {
+            conn.close();
+            call.close();
+        }catch (Exception e)
+        {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
+}
